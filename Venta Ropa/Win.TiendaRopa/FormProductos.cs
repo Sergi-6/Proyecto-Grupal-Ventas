a1cf4549d8434bee1f59c9dc7d4e3bd9ec91bf30
Sergi Flores <flores.sergy2262@gmail.com>
2021-06-24 21:00:34 -0600

    Creando fase 2 del proyecto

--- Venta Ropa/Win.TiendaRopa/FormProductos.cs
+++ Venta Ropa/Win.TiendaRopa/FormProductos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BL.Ventas;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,95 @@ namespace Win.TiendaRopa
 {
     public partial class FormProductos : Form
     {
+        ProductosBL _productos;
+
         public FormProductos()
         {
             InitializeComponent();
+
+            _productos = new ProductosBL();
+            listaProductosBindingSource.DataSource = _productos.ObtenerProductos();
+
+        }
+
+        private void FormProductos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listaProductosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaProductosBindingSource.EndEdit();
+            var producto = (Producto)listaProductosBindingSource.Current;
+
+            var resultado = _productos.GuardarProducto(producto);
+
+            if (resultado.Exitoso == true)
+            {
+                listaProductosBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _productos.AgregarProducto();
+            listaProductosBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtoncancelar.Visible = !valor;
+
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+
+            if (idTextBox.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+                }   
+            }
+        }
+
+        private void Eliminar(int id)
+        {
+            
+            var resultado = _productos.EliminarProducto(id);
+
+            if (resultado == true)
+            {
+                listaProductosBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar el producto");
+            }
+        }
+
+        private void toolStripButtoncancelar_Click(object sender, EventArgs e)
+        {
+            DeshabilitarHabilitarBotones(true);
+            Eliminar(0);
         }
     }
 }
